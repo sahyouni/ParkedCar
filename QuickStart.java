77fa59003b09cfc0e42dd98c56d36ddf20490bcb
unknown <sahyouni@gmail.com>
2020-01-28 19:55:35 -0800

    Create an instance and call a method

---
+++ QuickStart.java
@@ -0,0 +1,9 @@
+class QuickStart {
+    public static void main(String[] args) {
+        ParkedCar car = new ParkedCar("Rami", "Nissan", 2000, "FW952L", 5);
+
+        car.displayDetails();
+
+
+    }
+}
