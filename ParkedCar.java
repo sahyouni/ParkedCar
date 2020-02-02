76385ff78f8540be30fd40a2986a35140d617cb7
Rami Sahyouni <rami.sahyouni@trulioo.com>
2020-02-02 14:48:27 -0800

    adding accessors and mutators

--- ParkedCar.java
+++ ParkedCar.java
@@ -10,15 +10,68 @@ public class ParkedCar {
     private int minutesParked;
     private int currentYear = Calendar.getInstance().get(Calendar.YEAR);
 
-    public ParkedCar(String inputOwnerName, String inputCarMake, int inputModelYear,
-     String inputLicensePlateNumber, int inputMinutesParked) {
+    private void ThrowIfNull(String valueToValidate, String errorMessage) {
+        if (valueToValidate == null)
+            throw new IllegalArgumentException(errorMessage);
+    }
 
-        if (inputOwnerName == null) { throw new IllegalArgumentException("owner name cannot be null"); } 
-        if (inputCarMake == null) { throw new IllegalArgumentException("car make cannot be null"); } 
-        if (inputModelYear < 1900) { throw new IllegalArgumentException("year model cannot be earlier than 1900"); } 
-        if (inputModelYear > currentYear) { throw new IllegalArgumentException("year model cannot be later than 2020"); }
-        if (inputLicensePlateNumber == null) { throw new IllegalArgumentException("car license plate number cannot be null"); } 
-        if (inputMinutesParked <= 0) { throw new IllegalArgumentException("minutes parked should be greater than 0"); } 
+    public String GetOwnerName() {
+        return ownerName;
+    }
+
+    public void SetOwnerName(String value){
+        ThrowIfNull(value, "owner name cannot be null");
+        ownerName = value;
+    }
+
+    public String GetCarMake() {
+        return carMake;
+    }
+
+    public void SetCarMake(String value) {
+        ThrowIfNull(value, "car make cannot be null");
+        carMake = value;
+    }
+
+    public int GetModelYear() {
+        return modelYear;
+    }
+
+    public void SetModelYear(int value){
+        if(value <1900)
+        throw new IllegalArgumentException("year model cannot be earlier than 1900");
+
+        if(value > currentYear)
+        throw new IllegalArgumentException("year model cannot be later than 2020");
+
+        modelYear = value;
+    }
+
+    public String GetLicensePlateNumber() {
+        return carLicensePlateNumber;
+    }
+    public void SetLicensePlateNumber(String value){
+        ThrowIfNull(value, "car license plate number cannot be null");
+        carLicensePlateNumber = value;
+    }
+
+    public ParkedCar(String inputOwnerName, String inputCarMake, int inputModelYear, String inputLicensePlateNumber,
+            int inputMinutesParked) {
+
+        ThrowIfNull(inputOwnerName, "owner name cannot be null");
+        ThrowIfNull(inputCarMake, "car make cannot be null");
+        ThrowIfNull(inputLicensePlateNumber, "car license plate number cannot be null");
+
+        if (inputModelYear < 1900) {
+            throw new IllegalArgumentException("year model cannot be earlier than 1900");
+        }
+        if (inputModelYear > currentYear) {
+            throw new IllegalArgumentException("year model cannot be later than 2020");
+        }
+      
+        if (inputMinutesParked <= 0) {
+            throw new IllegalArgumentException("minutes parked should be greater than 0");
+        }
 
         ownerName = inputOwnerName;
         carMake = inputCarMake;
@@ -28,14 +81,17 @@ public class ParkedCar {
     }
 
     public void displayDetails() {
+        System.out.println("------------");
+
         System.out.println(ownerName);
         System.out.println(carMake);
         System.out.println(modelYear);
         System.out.println(carLicensePlateNumber);
         System.out.println(minutesParked);
-
-
         System.out.println(currentYear);
+
+        System.out.println("------------");
+
 
     }
 }
--- QuickStart.java
+++ QuickStart.java
@@ -1,9 +1,19 @@
 class QuickStart {
     public static void main(String[] args) {
         ParkedCar car = new ParkedCar("Rami", "Nissan", 2000, "FW952L", 5);
-
         car.displayDetails();
 
+        car.SetCarMake("Toyota");
+        car.displayDetails();
+
+        car.SetLicensePlateNumber("FFFFWWWWGGGG");
+        car.displayDetails();
+
+        car.SetModelYear(1999);
+        car.displayDetails();
+
+        car.SetOwnerName("Borat");
+        car.displayDetails();
 
     }
 }
