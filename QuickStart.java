a347abfbfb3853c5b728c2b7d2a2916d22a50455
Rami Sahyouni <ramilb80@hotmail.com>
2020-02-13 15:35:16 -0800

    more fun

--- QuickStart.java
+++ QuickStart.java
@@ -1,31 +1,35 @@
 class QuickStart {
     public static void main(String[] args) {
 
-        System.out.println("-----total number of sold books:" + BookClub.totalNumberOfSoldBooks + "---------");
+        PrintTotalNumberOfBooksSold();
 
         BookClub wackyJackie = new BookClub("Wacky Jackie", 3);
         PrintDetails(wackyJackie);
 
-        System.out.println(wackyJackie.getClientName() + "is about to set its purchased books to 5...");
         wackyJackie.setNumberOfPurchasedBooksPerMonth(5);
         PrintDetails(wackyJackie);
 
-        wackyJackie.setNumberOfPurchasedBooksPerMonth(5);
+        wackyJackie.setNumberOfPurchasedBooksPerMonth(10);
         PrintDetails(wackyJackie);
 
-        System.out.println("-----total number of sold books:" + BookClub.totalNumberOfSoldBooks + "---------");
-
+        PrintTotalNumberOfBooksSold();
         BookClub cherrysFruits = new BookClub("Cherry's fresh fruits and salads", 100);
         PrintDetails(cherrysFruits);
 
-        System.out.println("-----total number of sold books:" + BookClub.totalNumberOfSoldBooks + "---------");
+        PrintTotalNumberOfBooksSold();
 
         BookClub samsWorkOutRoutine = new BookClub("Build a six pack in two hours", 5);
         PrintDetails(samsWorkOutRoutine);
+
+        PrintTotalNumberOfBooksSold();
+    }
+
+    private static void PrintTotalNumberOfBooksSold(){
+        System.out.println("-----total number of sold books sold so far is " + BookClub.totalNumberOfSoldBooks + "---------");
     }
 
     private static void PrintDetails(BookClub bookClub) {
         System.out.println(bookClub.getClientName() + " book club has " + bookClub.calculateBookPoints()
-                + " points and has purchased " + bookClub.getNumberOfPurchasedBooksPerMonth() + " books per month");
+                + " points and has purchased " + bookClub.getNumberOfPurchasedBooksPerMonth() + " books per month.");
     }
 }
